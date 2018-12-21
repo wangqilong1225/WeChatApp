088b7392b6ea80cbb9f518f6022a38c11a6f339a
wangqilong <664660754@qq.com>
2018-12-21 11:32:04 +0800

    微信接口验证

---
+++ WeChatApp/Controllers/WeChatApiController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WeChatApp.Controllers
+{
+    /// <summary>
+    /// 接口验证控制
+    /// </summary>
+    public class WeChatApiController : Controller
+    {
+        // GET: WeChatApi
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        
+        /// <summary>
+        /// 进入地址 http://wangqilong.goho.co/WeChatApi/WeChat
+        /// </summary>
+        public void WeChat()
+        {
+
+        }
+    }
+}
