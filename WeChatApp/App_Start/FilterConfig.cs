4011b11989b22100f9d41a53f589a54ae9bbe6b6
wangqilong <664660754@qq.com>
2018-12-21 09:30:33 +0800

    添加项目文件。

---
+++ WeChatApp/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace WeChatApp
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
